aecdb5749940b8b818ce8b8b044c51c855db35ff
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-03-27 15:49:38 -0300

    teste api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia'
 
 new Elysia()
-  .get('/', { api: 'test api'})
+  .get('/', { api: 'test api 2023'})
   .get('/hello', 'Do you miss me?')
   .listen(3001, () => console.log(`Server is running on port http://localhost:3001`))
